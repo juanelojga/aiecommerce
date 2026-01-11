b7abdecb975c075ade2e2bc0faf18022cc4e36df
Juan Almeida <juanelojga@gmail.com>
2026-01-10 20:30:04 -0500

    refactor(mercadolibre): remove unused `MercadoLibrePriceEngine` import from module initialization

--- aiecommerce/services/mercadolibre_impl/__init__.py
+++ aiecommerce/services/mercadolibre_impl/__init__.py
@@ -1,10 +1,7 @@
-from aiecommerce.services.mercadolibre_category_impl.price import MercadoLibrePriceEngine
-
 from .client import MercadoLibreClient
 from .filter import MercadoLibreFilter
 
 __all__ = [
     "MercadoLibreClient",
     "MercadoLibreFilter",
-    "MercadoLibrePriceEngine",
 ]
