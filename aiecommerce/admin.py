c821dc9e2ab2e9bd8d02b1fc8c2805e4ac878b34
Juan Almeida <juanelojga@gmail.com>
2026-02-07 12:13:55 -0500

    Add custom admin filter for GTIN field in ProductMasterAdmin
    - Introduced `GtinNotNullFilter` to filter products based on GTIN presence.
    - Added the filter to `list_filter` in `ProductMasterAdmin`.

--- aiecommerce/admin.py
+++ aiecommerce/admin.py
@@ -1,13 +1,26 @@
 from django.contrib import admin
+from django.db.models import Q
 
 from aiecommerce.models import MercadoLibreListing, MercadoLibreToken, ProductDetailScrape, ProductImage, ProductMaster, ProductRawPDF, ProductRawWeb
 
 
-# Register your models
+class GtinNotNullFilter(admin.SimpleListFilter):
+    title = "GTIN"  # Display title in the filter UI
+    parameter_name = "gtin"
+
+    def lookups(self, request, model_admin):
+        return (("null", "Empty"),)
+
+    def queryset(self, request, queryset):
+        if self.value() == "null":
+            return queryset.filter(~Q(gtin__isnull=False))  # Filter where gtin is not null
+        return queryset
+
+
 @admin.register(ProductMaster)
 class ProductMasterAdmin(admin.ModelAdmin):
     list_display = ("code", "get_ml_id", "price", "is_active", "sku", "gtin", "is_for_mercadolibre", "last_updated")
-    list_filter = ("is_active", "category")
+    list_filter = ("is_active", "category", GtinNotNullFilter)  # Add the custom filter here
     search_fields = ("code", "description", "mercadolibre_listing__ml_id")
 
     def get_queryset(self, request):
